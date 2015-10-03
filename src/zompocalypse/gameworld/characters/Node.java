803b588a4c81a65ba76d5ff3e598c98c9bf6a802
KieranM <kieran__mckay@hotmail.com>
2015-10-03 17:13:00 +1300

    PathFinder and Node classes added for A* best path searching zombies. not yet complete

---
+++ src/zompocalypse/gameworld/characters/Node.java
@@ -0,0 +1,50 @@
+package zompocalypse.gameworld.characters;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import zompocalypse.gameworld.world.Floor;
+
+public class Node{
+
+	public final int nodeID;
+	public final Collection<Floor> floors;
+
+	//A* fields
+	private double cost = 0;
+	private Node pathFrom = null;
+	private boolean visited = false;
+	
+	public Node(int nodeID) {
+		this.nodeID = nodeID;
+		this.floors = new HashSet<Floor>();
+	}
+
+	public void addFloor(Floor floor) {
+		floors.add(floor);
+	}
+	
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+
+	public Node getPathFrom() {
+		return pathFrom;
+	}
+
+	public void setPathFrom(Node pathFrom) {
+		this.pathFrom = pathFrom;
+	}
+
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+}
---
+++ src/zompocalypse/gameworld/characters/PathFinder.java
@@ -0,0 +1,131 @@
+package zompocalypse.gameworld.characters;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.Stack;
+
+import zompocalypse.gameworld.world.Floor;
+
+public class PathFinder {
+	private Set<Node> visited = new HashSet<Node>();
+	private Stack<Floor> path = new Stack<Floor>();
+
+	private PriorityQueue<AStarNode> fringe;
+
+	private Node end;
+	private double estimate;
+	private AStarNode data;
+
+	public PathFinder(Node start, Node end) {
+		this.end = end;
+
+		estimate = estimate(start);
+		fringe = new PriorityQueue<AStarNode>();
+
+		data = new AStarNode(start, null, 0, estimate);
+		fringe.add(data);
+		findPath();
+	}
+
+	public void findPath() {
+		while (!fringe.isEmpty()) {
+			data = fringe.poll();
+			Node current = data.getNode();
+
+			if (!current.isVisited()) {
+				current.setVisited(true);
+				current.setPathFrom(data.getFrom());
+				current.setCost(data.getCostToHere());
+				visited.add(current);
+
+				if (current.equals(end)) {
+					Node temp = current;
+
+					while (temp.getPathFrom() != null) {
+						path.push(getConnectingFloor(temp, temp.getPathFrom()));
+						temp = temp.getPathFrom();
+					}
+					break;
+				}
+			/*	
+				for (Floor exit : current.floors) {
+					Node neigh = exit.end;
+					if (!neigh.isVisited()) {
+						double costToNeigh = current.getCost() + exit.length;
+						double estTotal = costToNeigh + estimate(neigh);
+						data = new AStarNode(neigh, current, costToNeigh,
+								estTotal);
+						fringe.add(data);
+					}
+				}
+				*/
+			}
+		}
+	}
+
+	public Floor getConnectingFloor(Node start, Node end) {
+		for (Floor exit : start.floors) {
+		//	if (exit.end.equals(end)) {
+				return exit;
+		//	}
+		}
+		return null;
+	}
+
+	public double estimate(Node current) {
+		//int xdif = end.
+		//return end.location.distance(current.location);
+				return 0;
+	}
+
+	public Set<Node> getVisited() {
+		return visited;
+	}
+
+	public Stack<Floor> getPath() {
+		return path;
+	}
+
+	private class AStarNode implements Comparable<AStarNode> {
+		private Node node;
+		private Node from;
+		private double costToHere;
+		private double totEstCost;
+
+		public AStarNode(Node node, Node from, double costToHere,
+				double totEstCost) {
+			this.node = node;
+			this.from = from;
+			this.costToHere = costToHere;
+			this.totEstCost = totEstCost;
+		}
+
+		public Node getNode() {
+			return node;
+		}
+
+		public Node getFrom() {
+			return from;
+		}
+
+		public double getCostToHere() {
+			return costToHere;
+		}
+
+		public double getTotEstCost() {
+			return totEstCost;
+		}
+
+		@Override
+		public int compareTo(AStarNode o) {
+			if (this.getTotEstCost() < o.getTotEstCost()) {
+				return -1;
+			}
+			if (this.getTotEstCost() > o.getTotEstCost()) {
+				return 1;
+			}
+			return 0;
+		}
+	}
+}
