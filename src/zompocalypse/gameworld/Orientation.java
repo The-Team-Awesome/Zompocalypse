211bf419feeab98141ceaa9cbd00992291b60c3c
David Thomsen <thomsedavi@yahoo.co.nz>
2015-10-06 21:48:28 +1300

    order of directions in get orientation method fixed

--- src/zompocalypse/gameworld/Orientation.java
+++ src/zompocalypse/gameworld/Orientation.java
@@ -6,20 +6,18 @@ public enum Orientation {
 	public static Orientation getCharacterOrientation(Orientation queued,
 			Orientation worldOrientation) {
 
-		int mer = (queued.ordinal() + +4 - worldOrientation.ordinal()) % 4;
-
-		// TODO this should be North, East, South, West, in that order!
-		// Temporarily this way because it draws 'correctly'.
+		// some sweet modulus action, I <3 maths
+		int mer = (worldOrientation.ordinal() - queued.ordinal() + 4) % 4;
 
 		switch (mer) {
 		case 0:
 			return NORTH;
 		case 1:
-			return WEST;
+			return EAST;
 		case 2:
 			return SOUTH;
 		case 3:
-			return EAST;
+			return WEST;
 		}
 
 		// Cannot possibly ever get here!
