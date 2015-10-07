e9bd938f056bef0e1aa2f45ad87417a3b881ad19
Danielle Emygdio <emygdio.d@gmail.com>
2015-10-07 19:35:39 +1300

    ZButton implementing different colors and forecolor depeding on the state of the button. Some panels with colors implemented.

--- src/zompocalypse/ui/appwindow/custom/CustomUtils.java
+++ src/zompocalypse/ui/appwindow/custom/CustomUtils.java
@@ -10,11 +10,15 @@ import java.awt.Color;
  * @author Sam Costigan
  */
 public class CustomUtils {
+	public static final Color radioSelected = new Color(0x5B0E1D);
 
-	public static final Color radioSelected = Color.RED;
 	public static final Color radioUnselected = Color.GRAY;
 
+	public static final Color textInButton = Color.WHITE;
+	public static final Color button = new Color(0x5B0E1D);
+	public static final Color buttonPressed = new Color(0x69323D);
+	public static final Color buttonRollover = new Color(0x8C2A3D);
+	public static final Color menuPanelBackground = new Color(0x967F2D);
 
-	public static final Color button = Color.RED;
-
+	public static final Color buttonUnabled =  new Color(0x7E78B4);
 }
