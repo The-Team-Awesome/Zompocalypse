3d39fcd431b99115ce4d27cd1a22c1952c96fce5
KieranM <kieran__mckay@hotmail.com>
2015-10-13 12:53:48 +1300

    registering a player now constructs player with chosen characters name

--- src/zompocalypse/gameworld/world/World.java
+++ src/zompocalypse/gameworld/world/World.java
@@ -270,7 +270,7 @@ public class World implements Serializable {
 		// TODO: This should really get valid information for name,
 		// as well as select their x, y co-ordinates based on a valid portal
 		Player player = new Player(x, y, Orientation.NORTH, ++id, 0,
-				"Bibbly Bob", filenames, this);
+				fileName, filenames, this);
 		idToActor.put(id, player);
 		objects[player.getX()][player.getY()].add(player);
 
