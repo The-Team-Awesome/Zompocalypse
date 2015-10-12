b83d9bdead17610782f80d0da26b99288566b651
Pauline Kelly <pauline.m.kelly1@gmail.com>
2015-10-12 13:51:28 +1300

    commenting weapon class

--- src/zompocalypse/gameworld/items/Weapon.java
+++ src/zompocalypse/gameworld/items/Weapon.java
@@ -2,7 +2,6 @@ package zompocalypse.gameworld.items;
 
 import java.awt.Graphics;
 import java.awt.Image;
-import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -12,14 +11,22 @@ import zompocalypse.gameworld.Orientation;
 import zompocalypse.gameworld.characters.Player;
 import zompocalypse.gameworld.world.World;
 
+/** Weapons are used to damage enemies.
+ *
+ *
+ * @author Sam Costigan, Kieran McKay
+ *
+ */
 public class Weapon implements Item {
 
+	private static final long serialVersionUID = 1L;
 	private transient Image currentImage;
 	private String filename;
 	private int uid;
 
 	private int strength;
 	private String description;
+	private final int OFFSET_X = -10;
 
 	public Weapon(String filename, String description, int uid, int strength) {
 		this.filename = filename;
@@ -44,7 +51,7 @@ public class Weapon implements Item {
 
 	@Override
 	public void draw(int x, int y, Graphics g, Orientation worldOrientation) {
-		g.drawImage(currentImage, x+10, y, null);
+		g.drawImage(currentImage, x+OFFSET_X, y, null);
 	}
 
 	@Override
@@ -52,7 +59,7 @@ public class Weapon implements Item {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
+
 	@Override
 	public boolean occupiable(){
 		return true;
@@ -61,12 +68,12 @@ public class Weapon implements Item {
 	@Override
 	public void use(Player player) {
 		List<Item> inventory = player.getInventory();
-		
+
 		if(!inventory.contains(this)) {
 			player.pickUp(this);
 			World world = player.getWorld();
 			PriorityQueue<GameObject>[][] objects = world.getObjects();
-			
+
 			// TODO: This would be much nicer if objects could be retrieved from a map of ids to GameObjects
 			for(int x = 0; x < objects.length; x++) {
 				for(int y = 0; y < objects[0].length; y++) {
@@ -81,7 +88,7 @@ public class Weapon implements Item {
 			if(player.getEquipped() != null) {
 				player.pickUp(player.getEquipped());
 			}
-			
+
 			player.setEquipped(this);
 			inventory.remove(this);
 		}
@@ -101,5 +108,4 @@ public class Weapon implements Item {
 	public String examine() {
 		return description;
 	}
-
 }
