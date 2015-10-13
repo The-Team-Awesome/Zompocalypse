a38d90f7e2ac2dffd39304554c3128d13d9a3be6
KieranM <kieran__mckay@hotmail.com>
2015-10-13 17:25:02 +1300

    Javadoc ActorType

--- src/zompocalypse/gameworld/characters/ActorType.java
+++ src/zompocalypse/gameworld/characters/ActorType.java
@@ -1,5 +1,9 @@
 package zompocalypse.gameworld.characters;
-
+/**
+ * An enum for determining the type of an Actor
+ * 
+ * @author Kieran Mckay, 300276166
+ */
 public enum ActorType {
 	RANDOMZOMBIE, HOMERZOMBIE, PLAYER, NPC, SHOPKEEPER
 }
