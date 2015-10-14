7dd00d6ee449eff68803e4757243189f6fc531f7
Sam Costigan <samdcostigan@gmail.com>
2015-10-14 18:13:33 +1300

    #11, Adding empty test classes to be filled in

---
+++ src/zompocalypse/tests/logic/TorchTests.java
@@ -0,0 +1,37 @@
+package zompocalypse.tests.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import zompocalypse.gameworld.items.Door;
+
+public class TorchTests {
+
+	/*String[] test = {"wall_grey_1_door_closed_ew.png", "wall_grey_1_door_closed_ns.png"};
+
+	@Test public void doorValidUnlockTest1() {
+		Door d = new Door(0, 0, test, 55, true, 0);
+
+		assertTrue(d.unlock(true));
+	}
+
+	@Test public void doorValidUnlockTest2() {
+		Door d = new Door(0, 0, test, 55, false, 0);
+
+		assertTrue(d.unlock(true));
+	}
+
+	@Test public void doorInvalidUnlockTest1() {
+		Door d = new Door(0, 0, test, 55, true, 0);
+
+		assertFalse(d.unlock(false));
+	}
+
+	@Test public void doorInvalidUnlockTest2() {
+		Door d = new Door(0, 0, test, 55, false, 0);
+
+		assertFalse(d.unlock(false));
+	}*/
+
+}
